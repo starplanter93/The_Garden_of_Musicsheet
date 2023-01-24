a2991697c7fd4deede5af72754e2e896995aa552
ethanJcoding <ethan.95j@gmail.com>
2023-01-24 17:56:14 +0900

    Setting: EC2 데이터베이스 연동

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
