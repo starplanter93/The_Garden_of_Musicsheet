392123dadf2e00cc48272ed776e4877886565f45
starplanter93 <starplanter93@gmail.com>
2023-01-24 16:27:10 +0900

    Setting: eslint설정 변경

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -6,10 +6,8 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
     'prettier',
   ],
   overrides: [],
