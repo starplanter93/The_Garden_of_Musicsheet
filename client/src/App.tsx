39e4c17ad78632fa6a6fe0a1be8d8e3e3cc8bee2
starplanter93 <starplanter93@gmail.com>
2023-01-24 15:33:08 +0900

    admin push test1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test123456
+          Learn React test1234567
         </a>
       </header>
     </div>
