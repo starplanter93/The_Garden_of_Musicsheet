e57aaf52edada6880e8f9f749c74a3c576a84dbd
starplanter93 <starplanter93@gmail.com>
2023-01-24 14:39:07 +0900

    commit test1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test1
+          Learn React test12
         </a>
       </header>
     </div>
