3de23eef146ba27da4feb4d9eb0473fc3eb76faf
starplanter93 <starplanter93@gmail.com>
2023-01-24 16:19:22 +0900

    Setting: prettier/@typescript-eslint 익스텐드를 prettier로 교체

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
+    'prettier',
   ],
   overrides: [],
   parser: '@typescript-eslint/parser',
