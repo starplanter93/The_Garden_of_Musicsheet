b160afdf0349237c45092ea1d4d519f1056ccbeb
starplanter93 <starplanter93@gmail.com>
2023-01-24 16:14:10 +0900

    Setting: eslint, prettier 설정 변경

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -6,9 +6,11 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
   ],
   overrides: [],
   parser: '@typescript-eslint/parser',
