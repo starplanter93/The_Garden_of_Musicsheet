7e82eca97faf258a12595123d336072abb4c3278
ethanJcoding <ethan.95j@gmail.com>
2023-01-27 13:51:36 +0900

    Setting: .env 제거 안됨이슈 해결

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma.service';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
