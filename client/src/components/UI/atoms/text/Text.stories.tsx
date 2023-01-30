260d800a52c4fa5fbbc01ab3597ce44b04263371
starplanter93 <starplanter93@gmail.com>
2023-01-30 20:43:15 +0900

    Feat: Text Atoms 구현

---
+++ client/src/components/UI/atoms/text/Text.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Text from './Text';
+
+export default {
+  title: 'Atoms/Text',
+  component: Text,
+} as ComponentMeta<typeof Text>;
+
+const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
+
+export const FontOptions = Template.bind({});
+FontOptions.args = {
+  size: 'm',
+  weight: 'regular',
+  color: 'black',
+  children: 'Text',
+};
