4d9746106446b2ff828246be3e1c1b0b61c24e46
seungmin2ee <tmdals2815@gmail.com>
2023-01-31 01:57:34 +0900

    Feat: Icon size, color에 대한 props 타입 정의

--- client/src/components/UI/atoms/Icon/Icon.tsx
+++ client/src/components/UI/atoms/Icon/Icon.tsx
@@ -2,9 +2,15 @@ import * as icons from './icons';
 
 interface IconProps {
   icon: keyof typeof icons;
+  size?: 'xl' | 'l' | 'm' | 's' | 'xs';
+  color?: 'black' | 'gray' | 'green' | 'lightgray' | 'red' | 'white';
 }
 
-const Icon = ({ icon }: IconProps) => {
+const Icon = ({
+  icon = 'FaGuitar',
+  size = 'm',
+  color = 'black',
+}: IconProps) => {
   const SVG = icons[icon];
 
   return <SVG />;
