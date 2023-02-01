cd24ac09c927dd660d96f962f9597890a81aae35
starplanter93 <starplanter93@gmail.com>
2023-02-01 15:34:06 +0900

    Setting: 디렉토리 명 수정 text -> Text

--- client/src/components/UI/atoms/index.ts
+++ client/src/components/UI/atoms/index.ts
@@ -1,5 +1,5 @@
 import Icon from './Icon/Icon';
-import Text from './text/Text';
+import Text from './Text/Text';
 import ImgLayout from './ImgLayout/ImgLayout';
 
 export { Icon, Text, ImgLayout };
