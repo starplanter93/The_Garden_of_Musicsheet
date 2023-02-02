a70b1dd9e3f2f324c35bd356a6022bcfc91bdfe0
seungmin2ee <tmdals2815@gmail.com>
2023-02-02 15:06:08 +0900

    Feat: InstrumentCard Molecule props 타입 정의

--- client/src/components/UI/molecules/InstrumentCard/InstrumentCard.tsx
+++ client/src/components/UI/molecules/InstrumentCard/InstrumentCard.tsx
@@ -2,20 +2,26 @@ import classNames from 'classnames/bind';
 import { ImgLayout, Text } from '../../atoms';
 import styles from './instrumentCard.module.scss';
 
-const InstrumentCard = () => {
+interface InstrumentCardProps {
+  src: string;
+  type: string;
+  sheets: string;
+}
+
+const InstrumentCard = ({ src, type, sheets }: InstrumentCardProps) => {
   const cx = classNames.bind(styles);
 
   return (
     <ul className={cx('instrument-card')}>
       <li className={cx('instrument-img')}>
-        <ImgLayout shape="square" size="lg" alt="악기사진" />
+        <ImgLayout shape="square" size="responsive" src={src} alt="악기사진" />
       </li>
       <li className={cx('instrument-type')}>
-        <Text size="xlg">피아노</Text>
+        <Text size="xlg">{type}</Text>
       </li>
       <li className={cx('instrument-sheet')}>
         <Text size="m" color="gray" weight="medium">
-          악보 (55074)
+          {`악보 (${sheets})`}
         </Text>
       </li>
     </ul>
