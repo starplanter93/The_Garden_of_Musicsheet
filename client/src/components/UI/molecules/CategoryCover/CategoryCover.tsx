6a8324f94a1b1db694f3f4d90e89e5cccdd1d570
seungmin2ee <tmdals2815@gmail.com>
2023-02-02 18:49:11 +0900

    Feat: CategoryCover Molecule props 타입 정의

--- client/src/components/UI/molecules/CategoryCover/CategoryCover.tsx
+++ client/src/components/UI/molecules/CategoryCover/CategoryCover.tsx
@@ -2,27 +2,39 @@ import classNames from 'classnames/bind';
 import { ImgLayout, Text } from '../../atoms';
 import styles from './categoryCover.module.scss';
 
-const CategoryCover = () => {
+interface CategoryCoverProps {
+  category: string;
+  thumbnail: string;
+  title: string;
+  artist: string;
+}
+
+const CategoryCover = ({
+  category,
+  thumbnail,
+  title,
+  artist,
+}: CategoryCoverProps) => {
   const cx = classNames.bind(styles);
 
   return (
     <div className={cx('category-cover')}>
       <div className={cx('cover-img')}>
-        <ImgLayout shape="square" size="lg" alt="커버 이미지" />
+        <ImgLayout shape="square" size="lg" src={thumbnail} alt="커버 이미지" />
       </div>
       <div className={cx('cover-info')}>
         <ul>
           <li className={cx('info-category')}>
-            <Text size="lg">곡</Text>
+            <Text size="lg">{category}</Text>
           </li>
           <li className={cx('info-title')}>
             <Text size="txlg" weight="bold">
-              사건의 지평선
+              {title}
             </Text>
           </li>
           <li className={cx('info-artist')}>
             <Text size="lg" color="gray">
-              윤하
+              {artist}
             </Text>
           </li>
         </ul>
