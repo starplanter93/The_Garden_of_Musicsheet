50ad3e7c7b7c4a2386eeb20f1602837a9c103f28
seungmin2ee <tmdals2815@gmail.com>
2023-02-03 21:31:14 +0900

    Feat: molecule.ts에 Menu 추가

--- client/src/components/UI/molecules/index.ts
+++ client/src/components/UI/molecules/index.ts
@@ -2,5 +2,6 @@ import InstrumentCard from './InstrumentCard/InstrumentCard';
 import CategoryCover from './CategoryCover/CategoryCover';
 import MainScoreList from './MainScoreList/MainScoreList';
 import SongTitle from './SongTitle/SongTitle';
+import Menu from './GNB/Menu/Menu';
 
-export { InstrumentCard, CategoryCover, MainScoreList, SongTitle };
+export { InstrumentCard, CategoryCover, MainScoreList, SongTitle, Menu };
