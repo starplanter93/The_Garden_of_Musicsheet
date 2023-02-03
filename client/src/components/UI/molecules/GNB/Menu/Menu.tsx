27f30456b394c99693f845f475b0314451251889
seungmin2ee <tmdals2815@gmail.com>
2023-02-03 21:19:29 +0900

    Feat: Menu molecule 레이아웃 구현

---
+++ client/src/components/UI/molecules/GNB/Menu/Menu.tsx
@@ -0,0 +1,35 @@
+import styles from './menu.module.scss';
+import classNames from 'classnames/bind';
+import { Button, Icon, Text } from '../../../atoms';
+
+// Todo: active 클래스명 붙이는 조건 추가
+const Menu = () => {
+  const cx = classNames.bind(styles);
+
+  return (
+    <ul className={cx('menu-lists')}>
+      <li className={cx('menu-list', 'active')}>
+        <Button theme="transparent" size="auto">
+          <>
+            <Icon icon="MdOutlineQueueMusic" size="m" color="gray" />
+            <Text size="lg" color="gray">
+              곡
+            </Text>
+          </>
+        </Button>
+      </li>
+      <li className={cx('menu-list')}>
+        <Button theme="transparent" size="auto">
+          <>
+            <Icon icon="MdPiano" size="s" color="gray" />
+            <Text size="lg" color="gray">
+              악기
+            </Text>
+          </>
+        </Button>
+      </li>
+    </ul>
+  );
+};
+
+export default Menu;
