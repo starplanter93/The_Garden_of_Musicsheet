4ea7da58938d4bef1fb950bbd7b0324ee1cadf63
starplanter93 <starplanter93@gmail.com>
2023-02-07 17:06:45 +0900

    Setting: 파일 순서 변경

---
+++ client/src/assets/carouselImg.tsx
@@ -0,0 +1,11 @@
+import carouselBg1 from './carousel_bg1.png';
+import carouselBg2 from './carousel_bg2.png';
+import carouselBg3 from './carousel_bg3.png';
+import albumImg1 from './album_img1.png';
+import albumImg2 from './album_img2.png';
+import albumImg3 from './album_img3.png';
+
+const carouselBgs = [carouselBg1, carouselBg2, carouselBg3];
+const albumImgs = [albumImg1, albumImg2, albumImg3];
+
+export { carouselBgs, albumImgs };
