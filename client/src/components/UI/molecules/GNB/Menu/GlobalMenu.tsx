8d35a1b7ce90de96356e8cd8b2ae31a023c4ca34
seungmin2ee <tmdals2815@gmail.com>
2023-02-07 22:19:01 +0900

    Refactor: 메인 메뉴를 Button 대신 Link로 변경

--- client/src/components/UI/molecules/GNB/Menu/GlobalMenu.tsx
+++ client/src/components/UI/molecules/GNB/Menu/GlobalMenu.tsx
@@ -1,6 +1,7 @@
 import styles from './globalmenu.module.scss';
 import classNames from 'classnames/bind';
-import { Button, Icon, Text } from '../../../atoms';
+import { Icon, Text } from '../../../atoms';
+import { Link } from 'react-router-dom';
 
 // Todo: active 클래스명 붙이는 조건 추가
 const GlobalMenu = () => {
@@ -9,24 +10,20 @@ const GlobalMenu = () => {
   return (
     <ul className={cx('menu-lists')}>
       <li className={cx('menu-list', 'active')}>
-        <Button theme="transparent" size="auto">
-          <>
-            <Icon icon="MdOutlineQueueMusic" size="m" color="gray" />
-            <Text size="lg" color="gray">
-              곡
-            </Text>
-          </>
-        </Button>
+        <Link to="/">
+          <Icon icon="MdOutlineQueueMusic" size="m" color="gray" />
+          <Text size="lg" color="gray">
+            곡
+          </Text>
+        </Link>
       </li>
       <li className={cx('menu-list')}>
-        <Button theme="transparent" size="auto">
-          <>
-            <Icon icon="MdPiano" size="s" color="gray" />
-            <Text size="lg" color="gray">
-              악기
-            </Text>
-          </>
-        </Button>
+        <Link to="/instrument">
+          <Icon icon="MdPiano" size="s" color="gray" />
+          <Text size="lg" color="gray">
+            악기
+          </Text>
+        </Link>
       </li>
     </ul>
   );
