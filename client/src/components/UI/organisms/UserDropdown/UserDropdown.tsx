3884afa647941cca8197cd235253bce9b9329b92
seungmin2ee <tmdals2815@gmail.com>
2023-02-09 05:24:57 +0900

    Feat: UserDropdown 외부 영역 클릭시 닫는 기능 구현

--- client/src/components/UI/organisms/UserDropdown/UserDropdown.tsx
+++ client/src/components/UI/organisms/UserDropdown/UserDropdown.tsx
@@ -1,9 +1,26 @@
 import styles from './userDropdown.module.scss';
 import classNames from 'classnames/bind';
 import { MiniProfile, UserMenu } from '../../molecules';
+import { Dispatch, SetStateAction, useEffect } from 'react';
 
-const UserDropdown = () => {
+interface Props {
+  closeDropdownTarget: any;
+  setDropdown: Dispatch<SetStateAction<boolean>>;
+}
+
+const UserDropdown = ({ closeDropdownTarget, setDropdown }: Props) => {
   const cx = classNames.bind(styles);
+
+  const handleDropdownClose = (event: any) => {
+    if (!closeDropdownTarget.current.contains(event.target)) setDropdown(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleDropdownClose);
+    return () => {
+      document.removeEventListener('click', handleDropdownClose);
+    };
+  }, []);
 
   return (
     <div className={cx('dropdown-wrapper')}>
