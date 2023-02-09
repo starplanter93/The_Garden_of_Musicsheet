676d4bea40f2b4e6dcd28e64e7c0f58bf9f54cc6
starplanter93 <starplanter93@gmail.com>
2023-02-09 17:15:29 +0900

    Feat: MainPage 추가

---
+++ client/src/components/pages/Main/Main.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { MainGrid } from '../../UI/organisms';
+import { Carousel } from '../../UI/organisms';
+
+function Main() {
+  return (
+    <>
+      <Carousel />
+      <MainGrid
+        songTitle="노래 제목"
+        singer="가수"
+        scoreName="조금길지도모르는 악보제목"
+        scoreWriter="악보제작자"
+        instrument="사용된 악기"
+        difficulty="난이도"
+        price="3,000원"
+      />
+    </>
+  );
+}
+
+export default Main;
