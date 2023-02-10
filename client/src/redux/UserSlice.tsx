2e89e3afbb7b2014177faeae1c39c0147e4cf5d9
seungmin2ee <tmdals2815@gmail.com>
2023-02-11 04:02:17 +0900

    Feat: 로그인시 유저정보를 받아와 스토리지의 리덕스 상태에 저장

--- client/src/redux/UserSlice.tsx
+++ client/src/redux/UserSlice.tsx
@@ -1,30 +1,32 @@
 import { createSlice, PayloadAction, Reducer } from '@reduxjs/toolkit';
-// 나중에 유저 정보를 저장할 때 사용할 용도 지금은 사용 X
 
-// initalState 타입 정의
 interface StateType {
-  username: string;
+  displayName: string;
+  email: string;
+  phoneNumber: string;
+  photoURL: string;
 }
 
-// initalState 생성
-const initialState: any = '';
+const initialState: StateType = {
+  displayName: '',
+  email: '',
+  phoneNumber: '',
+  photoURL: '',
+};
 
-// 슬라이스생성
 export const userSlice = createSlice({
   name: 'userSlice',
   initialState,
   reducers: {
-    userInfo: (state: any, action: PayloadAction<any>) => {
+    userInfo: (state: StateType, action: PayloadAction<StateType>) => {
       state = action.payload;
       return state;
     },
   },
 });
 
-// 액션을 export 해준다.
 export const { userInfo } = userSlice.actions;
 
-// 슬라이스를 export 해준다.
 const userReducer: Reducer<typeof initialState> = userSlice.reducer;
 
 export default userReducer;
