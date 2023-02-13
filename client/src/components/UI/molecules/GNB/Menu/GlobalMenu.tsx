7855b6943c44ab075b933b197154dc04872c92ed
seungmin2ee <tmdals2815@gmail.com>
2023-02-14 05:05:31 +0900

    Design: Header 메뉴 active 효과 제거 및 hover 효과 적용

--- client/src/components/UI/molecules/GNB/Menu/GlobalMenu.tsx
+++ client/src/components/UI/molecules/GNB/Menu/GlobalMenu.tsx
@@ -1,28 +1,23 @@
 import styles from './globalmenu.module.scss';
 import classNames from 'classnames/bind';
 import { Icon, Text } from '../../../atoms';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const GlobalMenu = () => {
   const cx = classNames.bind(styles);
-  const { pathname } = useLocation();
 
   return (
     <ul className={cx('menu-lists')}>
-      <li className={cx('menu-list', pathname === '/' && 'active')}>
+      <li className={cx('menu-list')}>
         <Link to="/">
-          <Icon icon="MdOutlineQueueMusic" size="m" color="gray" />
-          <Text size="lg" color="gray">
-            곡
-          </Text>
+          <Icon icon="MdOutlineQueueMusic" />
+          <Text size="lg">곡</Text>
         </Link>
       </li>
-      <li className={cx('menu-list', pathname === '/instrument' && 'active')}>
+      <li className={cx('menu-list')}>
         <Link to="/instrument">
-          <Icon icon="MdPiano" size="s" color="gray" />
-          <Text size="lg" color="gray">
-            악기
-          </Text>
+          <Icon icon="MdPiano" size="s" />
+          <Text size="lg">악기</Text>
         </Link>
       </li>
     </ul>
