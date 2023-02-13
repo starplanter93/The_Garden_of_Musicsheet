08909124f3f431da40ad4519d0a54dccc19037a4
seungmin2ee <tmdals2815@gmail.com>
2023-02-13 18:59:25 +0900

    Feat: ScoreList의 버튼 UI 분기 및 스타일 수정

--- client/src/components/UI/molecules/ScoreList/ScoreList.tsx
+++ client/src/components/UI/molecules/ScoreList/ScoreList.tsx
@@ -1,12 +1,19 @@
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import { Text } from '../../atoms';
+import EditOrDownButton from '../EditOrDownButton/EditOrDownButton';
 import styles from './scoreList.module.scss';
 
-const ScoreList = () => {
+interface ScoreListProps {
+  btnType?: 'text' | 'edit' | 'download';
+}
+
+const ScoreList = ({ btnType = 'text' }: ScoreListProps) => {
   const cx = classNames.bind(styles);
+
   return (
     <div className={cx('wrapper')}>
+      {/* Todo: 경로 수정 */}
       <Link to="/scoredetail">
         <div className={cx('detail')}>
           <ul className={cx('score-song')}>
@@ -29,10 +36,16 @@ const ScoreList = () => {
             </li>
           </ul>
         </div>
+      </Link>
+      {btnType === 'text' ? (
         <div className={cx('price')}>
           <Text color="blue">5000원</Text>
         </div>
-      </Link>
+      ) : (
+        <div>
+          <EditOrDownButton event={btnType} />
+        </div>
+      )}
     </div>
   );
 };
