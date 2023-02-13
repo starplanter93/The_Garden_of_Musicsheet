e983e8ecd88f89766a702e8e7b61101497e177e7
seungmin2ee <tmdals2815@gmail.com>
2023-02-14 02:53:57 +0900

    Feat: InstrumentLists 더미데이터 연결

---
+++ client/src/components/UI/organisms/InstrumentLists/instrumentDummy.ts
@@ -0,0 +1,33 @@
+export const instrumentDummy = [
+  {
+    type: 'piano',
+    name: '피아노',
+    thumbnail:
+      'https://images.unsplash.com/photo-1520523839897-bd0b52f945a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
+  },
+
+  {
+    type: 'electric',
+    name: '일렉 기타',
+    thumbnail:
+      'https://images.unsplash.com/photo-1586391159312-e26d25fa4c42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80',
+  },
+  {
+    type: 'acoustic',
+    name: '어쿠스틱 기타',
+    thumbnail:
+      'https://images.unsplash.com/photo-1541689592655-f5f52825a3b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YWNvdXN0aWMlMjBndWl0YXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+  },
+  {
+    type: 'bass',
+    name: '베이스',
+    thumbnail:
+      'https://images.unsplash.com/photo-1609254757493-26972cd14571?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fGJhc3N8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+  },
+  {
+    type: 'drum',
+    name: '드럼',
+    thumbnail:
+      'https://images.unsplash.com/photo-1524230659092-07f99a75c013?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZHJ1bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+  },
+];
