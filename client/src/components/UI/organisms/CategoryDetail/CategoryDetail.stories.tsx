2c4cfc76774d29f62ea2d924b68a929dec8d585b
seungmin2ee <tmdals2815@gmail.com>
2023-02-17 02:57:12 +0900

    Test: CategoryDetail 스토리 args 초기값 수정

--- client/src/components/UI/organisms/CategoryDetail/CategoryDetail.stories.tsx
+++ client/src/components/UI/organisms/CategoryDetail/CategoryDetail.stories.tsx
@@ -12,10 +12,19 @@ const Template: ComponentStory<typeof CategoryDetail> = (args) => (
 
 export const Default = Template.bind({});
 Default.args = {
-  scoresByCategory: {
+  coverData: {
     thumbnail:
       'https://images.unsplash.com/photo-1520523839897-bd0b52f945a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80',
     name: '피아노',
-    scores: [],
   },
+  scoresByCategory: [
+    {
+      difficulty: '중간',
+      instType: '피아노',
+      songName: '오르트구름',
+      artist: '윤하',
+      author: 'lee',
+      price: '5000',
+    },
+  ],
 };
