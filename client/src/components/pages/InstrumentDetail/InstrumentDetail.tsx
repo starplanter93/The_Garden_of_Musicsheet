c56d5063f921484eee0130ec5ab0fb1ae9f82c97
seungmin2ee <tmdals2815@gmail.com>
2023-02-17 02:37:33 +0900

    Refactor: CategoryDetail props 수정으로 인한 악기 상세페이지 수정

--- client/src/components/pages/InstrumentDetail/InstrumentDetail.tsx
+++ client/src/components/pages/InstrumentDetail/InstrumentDetail.tsx
@@ -1,24 +1,37 @@
 import { DocumentData } from 'firebase/firestore/lite';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getScoresByInstrument } from '../../../firebase/firebase';
+import { getScoresByCategory } from '../../../firebase/firebase';
 import { CategoryDetail } from '../../UI/organisms';
 
 const InstrumentDetail = () => {
-  const [scoresByInst, setScoresByInst] = useState<DocumentData>({});
+  const [scoresByInst, setScoresByInst] = useState<DocumentData>([]);
+  const [coverData, setCoverData] = useState<DocumentData>({});
   const [totalLists, setTotalLists] = useState(0);
   const { instType } = useParams();
 
   useEffect(() => {
-    getScoresByInstrument(`${instType}`)
+    getScoresByCategory('instrument', `${instType}`)
       .then((data) => {
-        setScoresByInst(data);
+        setCoverData({
+          name: data.name || data.songName,
+          thumbnail: data.thumbnail || data.albumImg,
+          artist: data.artist,
+        });
+        setScoresByInst(data.scores);
         setTotalLists(data.scores.length);
       })
       .catch((error) => console.log(error));
   }, []);
 
-  return <CategoryDetail scoresByInst={scoresByInst} totalLists={totalLists} />;
+  return (
+    <CategoryDetail
+      category="악기"
+      coverData={coverData}
+      scoresByCategory={scoresByInst}
+      totalLists={totalLists}
+    />
+  );
 };
 
 export default InstrumentDetail;
