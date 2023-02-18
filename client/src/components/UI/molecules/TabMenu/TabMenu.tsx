59a79c536b3072ad5784d6386808d4a32bc37385
seungmin2ee <tmdals2815@gmail.com>
2023-02-19 05:42:03 +0900

    Design: 상세페이지 탭메뉴 active 애니메이션 변경

--- client/src/components/UI/molecules/TabMenu/TabMenu.tsx
+++ client/src/components/UI/molecules/TabMenu/TabMenu.tsx
@@ -27,30 +27,18 @@ const TabMenu = ({
     <div className={cx('wrapper')}>
       <ul className={cx('tabs')}>
         {tabGroupArr.map((tab, idx) => (
-          <li
-            key={idx}
-            className={cx(
-              'tab',
-              `tab${idx + 1}`,
-              currentTab === idx && 'active'
-            )}
-          >
+          <li key={idx} className={cx('tab', currentTab === idx && 'active')}>
             <Button
               theme="transparent"
               size="auto"
               onClick={() => handleClick(tab, idx)}
             >
-              <Text
-                size="lg"
-                color={currentTab === idx ? 'black' : 'gray'}
-                weight={currentTab === idx ? 'medium' : 'regular'}
-              >
+              <Text size="lg" color="gray">
                 {tab}
               </Text>
             </Button>
           </li>
         ))}
-        <li className={cx('triangle')}></li>
       </ul>
     </div>
   );
