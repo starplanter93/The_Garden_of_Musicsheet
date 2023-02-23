afc3411765bf9744280ecd27d228023b46cfdaf4
ethanJcoding <ethan.95j@gmail.com>
2023-02-23 15:03:10 +0900

    Feat: 유저네임 수정 구현

--- client/src/firebase/firebase.tsx
+++ client/src/firebase/firebase.tsx
@@ -100,6 +100,7 @@ export async function getMusicData(songName: string, data: StateType) {
     data.scores = [{ ...data.scores[0], scoreId: '0' }];
   } else {
     const lastScore = savedData.scores[savedData.scores.length - 1];
+    console.log(savedData.scores);
     const nextScoreId = parseInt(lastScore.scoreId) + 1;
     data = { ...data };
     data.scores = [{ ...data.scores[0], scoreId: nextScoreId.toString() }];
@@ -253,7 +254,95 @@ export async function getUserArticle(uid: string) {
 export async function userInitData(uid: string) {
   const ref = doc(db, 'user', uid);
   const snapshot = await getDoc(ref);
-  await setDoc(ref, { cash: '0' });
+  await setDoc(ref, { cash: '1000000', isActive: true });
 
   return snapshot.data();
+}
+
+export async function updateUserName(uid: string, newName: string) {
+  /** user 컬렉션 수정 */
+  const userRef = doc(db, 'user', uid);
+  const userSnap = await getDoc(userRef);
+  if (userSnap.exists()) {
+    const { posts } = userSnap.data();
+    console.log(posts);
+    const scoresWithId = posts.filter((obj: Score) => obj.authorId === uid);
+    scoresWithId.forEach((obj: Score) => (obj.author = newName));
+    await updateDoc(userRef, { posts: posts });
+  }
+
+  /** music 컬렉션 수정*/
+  const musicRef = collection(db, 'music');
+  const musicSnap = await getDocs(musicRef);
+  const musicList = musicSnap.docs.map((doc: DocumentData) => doc.data());
+
+  const musicArr = musicList.map((el) => el.scores);
+
+  for (let i = 0; i < musicArr.length; i++) {
+    for (let j = 0; j < musicArr[i].length; j++) {
+      if (musicArr[i][j].authorId === uid) {
+        const songName = `${musicArr[i][j].songName}-${musicArr[i][j].artist}`;
+        const infoRef = doc(db, 'music', songName);
+        const snapshot = await getDoc(infoRef);
+
+        if (snapshot.exists()) {
+          const { scores } = snapshot.data();
+          const scoresWithId = scores.filter(
+            (obj: Score) => obj.authorId === uid
+          );
+          scoresWithId.forEach((obj: Score) => (obj.author = newName));
+
+          await updateDoc(infoRef, { scores: scores });
+        }
+      }
+    }
+  }
+
+  /** instrument 컬렉션 수정*/
+  const instRef = collection(db, 'instrument');
+  const instSnap = await getDocs(instRef);
+  const instList = instSnap.docs.map((doc: DocumentData) => doc.data());
+
+  const instArr = instList.map((el) => el.scores);
+
+  for (let i = 0; i < instArr.length; i++) {
+    for (let j = 0; j < instArr[i].length; j++) {
+      if (instArr[i][j].authorId === uid) {
+        const data = instArr[i][j];
+        let inst = data.instType;
+        if (inst === '피아노') {
+          inst = 'piano';
+        }
+        if (inst === '어쿠스틱 기타') {
+          inst = 'acoustic';
+        }
+        if (inst === '베이스') {
+          inst = 'bass';
+        }
+        if (inst === '드럼') {
+          inst = 'drum';
+        }
+        if (inst === '일렉 기타') {
+          inst = 'electric';
+        }
+
+        const infoRef = doc(db, 'instrument', inst);
+        const snapshot = await getDoc(infoRef);
+
+        if (snapshot.exists()) {
+          const { scores } = snapshot.data();
+          const scoresWithId = scores.filter(
+            (obj: Score) => obj.authorId === uid
+          );
+          scoresWithId.forEach((obj: Score) => (obj.author = newName));
+
+          console.log(scoresWithId);
+
+          await updateDoc(infoRef, { scores: scores });
+        }
+      }
+    }
+  }
+
+  return userSnap.data();
 }
